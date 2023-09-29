6c439acfbaefe2ff37dc96af7ca5a1d2970b435d
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-09-29 17:50:11 +0530

    Add files via upload

---
+++ 8_Printing_1_N_using_backtracking.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int i, int n){
+    if(i<=0) return;
+    print(i-1, n);
+    cout<<i<<" ";
+}
+int main()
+{
+ int n;
+ cin>>n;
+ print(n,n);
+ return 0;
+}
